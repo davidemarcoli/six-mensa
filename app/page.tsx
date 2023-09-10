d5ec7e734abf7d04208db8bf5b36983c26e0af12
davidemarcoli <davide@marcoli.ch>
2023-09-10 21:11:32 +0200

    Improve styling and loading

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ export default async function Home() {
     const pdfDataUrls = await getPdfUrls();
 
     return (
-        <main className="flex min-h-screen items-center justify-between flex-row">
-            {pdfDataUrls.map((pdfDataUrl, i) => (
-                <embed key={i} src={pdfDataUrl} type="application/pdf" style={{ width: '100%', height: '100vh' }}></embed>
+        <main className="flex min-h-screen items-center justify-between flex-row p-5 bg-gray-900 text-white">
+            {pdfDataUrls.map((pdf, i) => (
+                <div key={i} className="w-full h-full p-5">
+                    <h2 className="text-lg font-semibold mb-5 uppercase tracking-wide shadow-md p-3 rounded bg-gray-800">
+                        {pdf.title}
+                    </h2>
+                    {/*<embed src={pdf.dataUrl} type="application/pdf" style={{ width: '100%', height: 'calc(100vh - 200px)' }}></embed>*/}
+                    <object data={pdf.dataUrl} type="application/pdf" style={{ width: '100%', height: 'calc(100vh - 200px)' }}></object>
+                </div>
             ))}
         </main>
     )
@@ -12,23 +18,19 @@ export default async function Home() {
 
 
 async function getPdfUrls() {
-    const pdfUrls = [
-        "https://www.betriebsrestaurants-migros.ch/media/x4vjg4pd/menueplan_six-ht201.pdf",
-        "https://www.betriebsrestaurants-migros.ch/media/k5dnh0sd/landingpage_menueplan_htp.pdf",
-    ]
+    const pdfData = [
+        { url: "https://www.betriebsrestaurants-migros.ch/media/x4vjg4pd/menueplan_six-ht201.pdf", title: "Menu Plan HT201" },
+        { url: "https://www.betriebsrestaurants-migros.ch/media/k5dnh0sd/landingpage_menueplan_htp.pdf", title: "Menu Plan HTP" }
+    ];
 
-    // fetch the pdfs
-    const pdfs = await Promise.all(pdfUrls.map(async (url) => {
-        const response = await fetch(url, {next: { revalidate: 60 * 60 * 24 }});
+    return await Promise.all(pdfData.map(async (data) => {
+        const response = await fetch(data.url);
         const blob = await response.blob();
         const buffer = await blob.arrayBuffer();
-        return Buffer.from(buffer).toString('base64');
+        const base64 = Buffer.from(buffer).toString('base64');
+        return {
+            title: data.title,
+            dataUrl: `data:application/pdf;base64,${base64}#zoom=100&view=FitW&toolbar=0`
+        };
     }));
-
-    // const response = await fetch(pdfUrls[0]);
-    // const blob = await response.blob();
-    // const buffer = await blob.arrayBuffer();
-    // const base64 = Buffer.from(buffer).toString('base64');
-
-    return pdfs.map((pdf) => `data:application/pdf;base64,${pdf}#page=1&zoom=100&view=FitH`);
 }
