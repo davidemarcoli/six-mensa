7f13c8e6ebfd197b50e9b794b6ad56f90ab735fa
Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>
2023-09-11 09:01:51 +0000

    First pdf prototype

--- app/page-working.tsx
+++ app/page-working.tsx
@@ -1,23 +1,19 @@
-import Image from "next/image";
-import { fromBase64 } from "pdf2pic";
-
 export default async function Home() {
   const pdfDataUrls = await getPdfUrls();
 
   return (
     <main className="flex min-h-screen items-center justify-between flex-row p-5 bg-gray-900 text-white">
-      {pdfDataUrls.map((element, i) => (
+      {pdfDataUrls.map((pdf, i) => (
         <div key={i} className="w-full h-full p-5">
           <h2 className="text-lg font-semibold mb-5 uppercase tracking-wide shadow-md p-3 rounded bg-gray-800">
-            {element.title}
+            {pdf.title}
           </h2>
           {/*<embed src={pdf.dataUrl} type="application/pdf" style={{ width: '100%', height: 'calc(100vh - 200px)' }}></embed>*/}
-          {/*<object
-            data={element.pdfDataUrl}
+          <object
+            data={pdf.dataUrl}
             type="application/pdf"
             style={{ width: "100%", height: "calc(100vh - 200px)" }}
-            ></object>*/}
-          <Image src={element.imageDataUrl} alt={element.title} width="100" height="100" style={{ width: "100%" }}></Image>
+          ></object>
         </div>
       ))}
     </main>
@@ -58,19 +54,10 @@ async function getPdfUrls() {
       const blob = await response.blob();
       const buffer = await blob.arrayBuffer();
       const base64 = Buffer.from(buffer).toString("base64");
-
-      const convert = fromBase64(base64, {
-        density: 500,
-        format: "png",
-        width: 3000,
-        height: 2000
-      })
-
-      return await {
+      return {
         title: data.title,
-        imageDataUrl: "data:image/png;base64," + (await convert(1, { responseType: "base64" })).base64,
-        pdfDataUrl: `data:application/pdf;base64,${base64}#zoom=100&view=FitW&toolbar=0`
-      }
+        dataUrl: `data:application/pdf;base64,${base64}#zoom=100&view=FitW&toolbar=0`,
+      };
     })
   );
 }
