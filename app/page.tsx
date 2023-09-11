34205ee6c924387ff2197e9d5220265c3e4c4389
StefanLaux <stefan.laux@edu.tbz.ch>
2023-09-11 11:12:13 +0200

    add weekdays but does not work with fetching data because useState has to be in Client Side File

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
+
 export default async function Home() {
   const pdfDataUrls = await getPdfUrls();
+  
+  useEffect(() => {
+    setSelectedDay(getCurrentWeekday());
+  }, []);
+
+  const getCurrentWeekday = () => {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDayIndex = new Date().getDay();
+    return days[currentDayIndex];
+  };
+
+  
+  const getAllWeekdays = () => {
+    return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  };
+
+  const [selectedDay, setSelectedDay] = useState(getCurrentWeekday());
+
 
   return (
-    <main className="flex min-h-screen items-center justify-between flex-row p-5 bg-gray-900 text-white">
+    <main className="flexSelf min-h-screen p-5 bg-gray-900 text-white">
+      <div className="changeWidth">
+<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Day</label>
+<select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={selectedDay} onChange={(e) => setSelectedDay(e.target.value)}>
+      {getAllWeekdays().map((day, i) => (
+        <option key={i} value={day}>
+          {day}
+        </option>
+      ))}
+    </select>
+</div>
+
+<div className="flex min-h-screen items-center justify-between flex-row p-5 bg-gray-900 text-white">
       {pdfDataUrls.map((pdf, i) => (
         <div key={i} className="w-full h-full p-5">
           <h2 className="text-lg font-semibold mb-5 uppercase tracking-wide shadow-md p-3 rounded bg-gray-800">
@@ -15,26 +47,12 @@ export default async function Home() {
             style={{ width: "100%", height: "calc(100vh - 200px)" }}
           ></object>
         </div>
-      ))}
+      ))}   
+</div>
     </main>
   );
 }
 
-function getCurrentWeekday() {
-  const currentDate = new Date();
-  const dayOfWeek = currentDate.getDay();
-  const weekdays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentWeekday = weekdays[dayOfWeek];
-  return currentWeekday;
-}
 
 async function getPdfUrls() {
   const pdfData = [
