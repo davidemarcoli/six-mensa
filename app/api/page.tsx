502602afd04bdd4bbf973b3a1ccf7e67d53a7ab7
davidemarcoli <davide@marcoli.ch>
2023-10-19 20:59:24 +0200

    Integrated the API into the Website Add shadcn-ui

---
+++ app/api/page.tsx
@@ -0,0 +1,63 @@
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+
+interface Menu {
+    day: string;
+    Local: string;
+    Vegi: string;
+    Globetrotter: string | undefined;
+    Buffet: string | undefined;
+}
+
+async function getMenuData(): Promise<Menu[]> {
+    return await fetch("http://server.davidemarcoli.dev:3000/-1").then((response) => response.json());
+}
+
+export default async function APIPage() {
+
+    const menuData = await getMenuData();
+
+    const currentDay = new Date().getDay() - 1;
+
+    let featuredMenus: Menu | undefined = undefined;
+    // if is weekday, show featured menu
+    if (currentDay > 0 && currentDay < 5) {
+        featuredMenus = menuData[currentDay];
+        menuData.splice(currentDay, 1);
+    }
+
+    return (
+        <>
+            <main className="flex h-full items-center justify-center flex-col p-3">
+                {featuredMenus && (
+                    <Card className="mb-4 mt-8">
+                        <CardHeader>
+                            <CardTitle><span className="underline">Heute</span> <span className="text-lg">({featuredMenus.day})</span></CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <p><b>Local:</b> {featuredMenus.Local}</p>
+                            <p><b>Vegi:</b> {featuredMenus.Vegi}</p>
+                            {featuredMenus.Globetrotter && <p><b>Globetrotter:</b> {featuredMenus.Globetrotter}</p>}
+                            {featuredMenus.Buffet && <p><b>Buffet:</b> {featuredMenus.Buffet}</p>}
+                        </CardContent>
+                    </Card>
+                )}
+
+                <div className="flex flex-wrap justify-center">
+                    {menuData.map((menu, i) => (
+                        <Card className="mr-2 mb-2"> {/* Add some margin for spacing between cards */}
+                            <CardHeader>
+                                <CardTitle>{menu.day}</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p><b>Local:</b> {menu.Local}</p>
+                                <p><b>Vegi:</b> {menu.Vegi}</p>
+                                {menu.Globetrotter && <p><b>Globetrotter:</b> {menu.Globetrotter}</p>}
+                                {menu.Buffet && <p><b>Buffet:</b> {menu.Buffet}</p>}
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+            </main>
+        </>
+    )
+}
---
+++ app/pdf/page.tsx
@@ -0,0 +1,40 @@
+export default async function PDFPage() {
+
+    const pdfDataUrls = await getPdfUrls();
+
+    return (
+        <>
+            <main className="flex h-full items-center justify-between flex-row p-3">
+                {pdfDataUrls.map((pdf, i) => (
+                    <div key={i} className="w-full h-full p-5">
+                        <h2 className="text-lg font-semibold mb-5 uppercase tracking-wide shadow-md p-3 rounded">
+                            {pdf.title}
+                        </h2>
+                        {/*<embed src={pdf.dataUrl} type="application/pdf" style={{ width: '100%', height: 'calc(100vh - 200px)' }}></embed>*/}
+                        <object data={pdf.dataUrl} type="application/pdf"
+                                style={{width: '100%', height: 'calc(100vh - 200px)'}}></object>
+                    </div>
+                ))}
+            </main>
+        </>
+    )
+}
+
+
+async function getPdfUrls() {
+    const pdfData = [
+        { url: "https://www.betriebsrestaurants-migros.ch/media/x4vjg4pd/menueplan_six-ht201.pdf", title: "Menu Plan HT201" },
+        { url: "https://www.betriebsrestaurants-migros.ch/media/k5dnh0sd/landingpage_menueplan_htp.pdf", title: "Menu Plan HTP" }
+    ];
+
+    return await Promise.all(pdfData.map(async (data) => {
+        const response = await fetch(data.url);
+        const blob = await response.blob();
+        const buffer = await blob.arrayBuffer();
+        const base64 = Buffer.from(buffer).toString('base64');
+        return {
+            title: data.title,
+            dataUrl: `data:application/pdf;base64,${base64}#zoom=100&view=FitW&toolbar=0`
+        };
+    }));
+}
