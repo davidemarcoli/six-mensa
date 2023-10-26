cbe852cb6c2bb15cc322c95ee7719ab402992b56
davidemarcoli <davide@marcoli.ch>
2023-10-26 19:44:23 +0200

    Improve PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+}
 
 const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
