199901c634fcfac9aab76d2403a7d562e52542b9
Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>
2023-11-07 15:00:04 +0000

    try making border visible

--- components/menu-card.tsx
+++ components/menu-card.tsx
@@ -65,7 +65,7 @@ export default function GenericMenuCard({menu, className, featured, menuItems}: 
     }, [menu]);
 
     return (
-        <Card className={`${className} ${featured ? 'border-solid' : ''}`}>
+        <Card className={`${className} ${featured ? 'border border-solid' : ''}`}>
             <CardHeader>
                 {featured && <CardTitle><span className="underline">Heute</span> <span
                     className="text-lg">({menu.day})</span></CardTitle>}
