5cf7dd338d1eb90b8175e462b2ec64d0a409ea86
Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>
2023-11-17 07:40:39 +0000

    Add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,19 @@ export default function RootLayout({
                     gtag('config', 'GA_MEASUREMENT_ID');
                     `}
                 </Script>
+                <Script id="google-tag-manager">
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5Z7TH786');`}
+                </Script>
             </head>
             <body className={`${inter.className}`}>
+                <noscript>
+                    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Z7TH786"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                </noscript>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
