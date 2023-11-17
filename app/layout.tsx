124ba3284b4ebb3b13ac88b00d7a625babfa0bd4
davidemarcoli <davide@marcoli.ch>
2023-11-17 09:36:30 +0100

    Replace Google Analytics with gdpr friendly self-hosted plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,29 +23,9 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning={true}>
             <head>
-                <Script src="https://www.googletagmanager.com/gtag/js?id=G-GXP0XVRRJY" />
-                <Script id="google-analytics">
-                    {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-            
-                    gtag('config', 'GA_MEASUREMENT_ID');
-                    `}
-                </Script>
-                <Script id="google-tag-manager">
-                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-5Z7TH786');`}
-                </Script>
+                <Script src="https://plausible.davidemarcoli.dev/js/script.js" data-domain="six-mensa.davidemarcoli.dev" />
             </head>
             <body className={`${inter.className}`}>
-                <noscript>
-                    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Z7TH786"
-                    height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe>
-                </noscript>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
